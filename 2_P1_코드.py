48b11d9215d299782e2b4ec1dd591bd190120b10
Ownfos <39623255+Ownfos@users.noreply.github.com>
2020-10-01 20:51:59 +0900

    Create 2_P1_코드.py

---
+++ 2_P1_코드.py
@@ -0,0 +1,19 @@
+class Chario:
+    def __init__(self):
+        print("haha")
+
+
+class Scanner:
+    def __init__(self):
+        print("haha")
+
+
+class Parser:
+    def __init__(self):
+        print("haha")
+
+
+if __name__ == "__main__":
+    chario = Chario()
+    scanner = Scanner()
+    parser = Parser()
