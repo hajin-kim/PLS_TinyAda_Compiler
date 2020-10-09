43b3ad797e4513c73e0d6191a6d91f3c9e9c4794
Hajin_Kim <kimhajin@yonsei.ac.kr>
2020-10-09 14:56:59 +0900

    add parsing units

---
+++ parser.py
@@ -0,0 +1,10 @@
+
+
+parser = (" ", ",", ":", ";", "..", "(", ")", ":=")
+reserved = ("is", "begin", "end", "type", "range ", "array", "of", "procedure", "in", "out", "if", "then", "elsif", "else", "end", "loop", "while", "exit", "when")
+
+relationalOperator = ("=" , "/=" , "<" , "<=" , ">" , ">=")
+binaryAddingOperator = ("+", "-")
+unaryAddingOperator = ("+", "-")
+multiplyingOperator = ("*", "/", "mod")
+
