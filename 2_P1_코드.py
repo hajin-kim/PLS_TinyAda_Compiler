2bc27ff89c5866c6bc4e849c52623f9f99714efe
Hajin_Kim <kimhajin@yonsei.ac.kr>
2020-10-10 00:46:55 +0900

    implement Parser

--- 2_P1_코드.py
+++ 2_P1_코드.py
@@ -33,6 +33,7 @@ class Chario:
 		"""
 		self.sourceFile = open(sourceFileName, 'rb')
 
+
 	def GetNextChar(self):
 		"""
 		Read a single character and convert it to lower case
@@ -42,6 +43,7 @@ class Chario:
 			return "EOF"
 		else:
 			return str(raw)[2:3].lower()
+
 
 	def PeekNextChar(self):
 		"""
@@ -130,6 +132,7 @@ class Scanner:
 		else:
 			self.chario.PrintErrorMessage("Unexpected symbol '" + result + "'")
 
+
 	def GetNextToken(self):
 		"""
 		Read characters from chario and return the first token found
@@ -195,18 +198,23 @@ class Parser:
 		Check whole subprogram is match with EBNF grammar for TinyAda
 		"""
 		self.subprogramSpecification()
-		self.accept(Token.IS, "\'" + Token.IS + "\' expected")
+		self.accept(Token.IS, 
+					"\'" + Token.IS + "\' expected")
 		self.declarativePart()
-		self.accept(Token.BEGIN, "\'" + Token.BEGIN + "\' expected")
+		self.accept(Token.BEGIN, 
+					"\'" + Token.BEGIN + "\' expected")
 		self.sequenceOfStatements()
-		self.accept(Token.END, "\'" + Token.END + "\' expected")
-		if token.code == Token.ID:	# force <procedure>identifier
+		self.accept(Token.END, 
+					"\'" + Token.END + "\' expected")
+		if self.token.code == Token.ID:	# force <procedure>identifier
 			self.token = scanner.GetNextToken()
 		self.accept(Token.SEMI, "semicolon expected")
 
+
 	def declarativePart(self):
-		while token.code in Token.basicDeclarationHandles:
+		while self.token.code in Token.basicDeclarationHandles:
 			self.basicDeclaration()
+
 
 	def basicDeclaration(self):
 		if self.token.code == Token.ID:
@@ -218,80 +226,114 @@ class Parser:
  		else:
  			self.fatalError("error in declaration part")
 
+
  	def numberOrObjectDeclaration(self):
  		self.identifierList()
- 		self.accept(Token.COLON,"\'" + Token.COLON + "\' expected")
+ 		self.accept(Token.COLON,
+ 					"\'" + Token.COLON + "\' expected")
  		if self.token.code == Token.CONSTANT:
  			self.numberDeclaration()
  		else:
  			self.objectDeclaration()
 
+
  	def objectDeclaration(self):
  		self.typeDefinition()
- 		self.accept(Token.SEMICOLON,"\'" + Token.SEMICOLON + "\' expected")
+ 		self.accept(Token.SEMICOLON, 
+ 					"\'" + Token.SEMICOLON + "\' expected")
+
 
  	def numberDeclaration(self);
- 		self.accept(Token.CONSTANT,"\'" + "constant" + "\' expected")
- 		self.accept(Token.COLON_EQ,"\'" + Token.COLON_EQ + "\' expected")
+ 		self.accept(Token.CONSTANT, 
+ 					"\'" + "constant" + "\' expected")
+ 		self.accept(Token.COLON_EQ, 
+ 					"\'" + Token.COLON_EQ + "\' expected")
  		self.expression()	# force <static>expression
- 		self.accept(Token.SEMICOLON,"\'" + Token.SEMICOLON + "\' expected")
+ 		self.accept(Token.SEMICOLON, 
+ 					"\'" + Token.SEMICOLON + "\' expected")
+
 
  	def identifierList(self):
- 		self.accept(Token.ID, "identifier expected")
+ 		self.accept(Token.ID, 
+ 					"identifier expected")
  		while self.token.code == Token.COMMA:
  			self.token = self.scanner.GetNextToken()
-			self.accept(Token.ID, "identifier expected")
+			self.accept(Token.ID, 
+						"identifier expected")
+
 
 	def typeDeclaration(self):
-		self.accept(Token.TYPE,"\'" + Token.TYPE + "\' expected")
-		self.accept(Token.ID, "identifier expected")
-		self.accept(Token.IS, "\'" + Token.IS + "\' expected")
+		self.accept(Token.TYPE, 
+					"\'" + Token.TYPE + "\' expected")
+		self.accept(Token.ID, 
+					"identifier expected")
+		self.accept(Token.IS, 
+					"\'" + Token.IS + "\' expected")
 		self.typeDefinition()
-		self.accept(Token.SEMICOLON,"\'" + Token.SEMICOLON + "\' expected")
+		self.accept(Token.SEMICOLON, 
+					"\'" + Token.SEMICOLON + "\' expected")
+
 
 	def typeDefinition(self):
-		if token.code == Token.PARENTHESIS_OPEN:
+		if self.token.code == Token.PARENTHESIS_OPEN:
 			self.enumerationTypeDefinition()
-		elif token.code == Token.ARRAY:
+		elif self.token.code == Token.ARRAY:
 			self.arrayTypeDefinition()
-		elif token.code == Token.RANGE:
+		elif self.token.code == Token.RANGE:
 			self.range()
-		else:
+		elif self.token.code == Token.ID:	# force <type>name
 			self.name()
+		else:
+ 			self.fatalError("error in type definition part")
+
 
 	def range(self):
-		self.accept(Token.RANGE, "\'" + Token.IS + "\' expected")
+		self.accept(Token.RANGE, 
+					"\'" + Token.IS + "\' expected")
 		self.simpleExpression()
-		self.accept(Token.DOT_DOT, "\'" + Token.DOT_DOT + "\' expected")
+		self.accept(Token.DOT_DOT, 
+					"\'" + Token.DOT_DOT + "\' expected")
 		self.simpleExpression()
+
 
 	def index(self):
-		if token.code == Token.RANGE:
+		if self.token.code == Token.RANGE:
 			self.range()
-		else:
+		elif self.token.code == Token.ID:	# force <type>name
 			self.name()
+		else:
+ 			self.fatalError("error in indexing")
+
 
 	def enumerationTypeDefinition(self):
-		self.accept(Token.PARENTHESIS_OPEN, "\'" + Token.PARENTHESIS_OPEN + "\' expected")
+		self.accept(Token.PARENTHESIS_OPEN, 
+					"\'" + Token.PARENTHESIS_OPEN + "\' expected")
 		self.identifierList()
-		self.accept(Token.PARENTHESIS_CLOSE, "\'" + Token.PARENTHESIS_CLOSE + "\' expected")
+		self.accept(Token.PARENTHESIS_CLOSE, 
+					"\'" + Token.PARENTHESIS_CLOSE + "\' expected")
 	
 	def arrayTypeDefinition(self):
-		self.accept(Token.ARRAY, "\'" + Token.ARRAY + "\' expected")
-		self.accept(Token.PARENTHESIS_OPEN, "\'" + Token.PARENTHESIS_OPEN + "\' expected")
+		self.accept(Token.ARRAY, 
+					"\'" + Token.ARRAY + "\' expected")
+		self.accept(Token.PARENTHESIS_OPEN, 
+					"\'" + Token.PARENTHESIS_OPEN + "\' expected")
 		self.index()
 		while self.token.code == Token.COMMA:
 			self.token = self.scanner.GetNextToken()
 			self.index()
-		self.accept(Token.PARENTHESIS_CLOSE, "\'" + Token.PARENTHESIS_CLOSE + "\' expected")
-		self.accept(Token.OF, "\'" + Token.OF + "\' expected")
-		self.name()
+		self.accept(Token.PARENTHESIS_CLOSE, 
+					"\'" + Token.PARENTHESIS_CLOSE + "\' expected")
+		self.accept(Token.OF, 
+					"\'" + Token.OF + "\' expected")
+		self.name()	# force <type>name
+
 
 	def subprogramSpecification(self):
 		self.accept(Token.PROC, "procedure expected!")
 		self.accept(Token.ID, "identifier expected")
 		if self.token.code == "(":	# note
 			self.formalPart()
+
 
 	def formalPart(self):
 		self.accept(Token.PARENTHESIS_OPEN, "\'" + Token.PARENTHESIS_OPEN + "\' expected")
@@ -300,6 +342,7 @@ class Parser:
 			self.token = self.scanner.GetNextToken()
 			self.parameterSpecification()
 		self.accept(Token.PARENTHESIS_CLOSE, "\'" + Token.PARENTHESIS_CLOSE + "\' expected")
+
 
 	def parameterSpecification(self):
 		self.identifierList()
@@ -312,6 +355,7 @@ class Parser:
 			self.token = self.scanner.GetNextToken()
 		if self.token.code == "OUT":
 			self.token = self.scanner.GetNextToken()
+
 
 	def sequenceOfStatements(self):
 		self.statement()
