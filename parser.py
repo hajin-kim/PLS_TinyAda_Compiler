8aabab52d3f71b30f0149580e76ff10dc0096805
hunny7434 <55790304+hunny7434@users.noreply.github.com>
2020-10-09 15:02:39 +0900

    end 삭제

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 
 
 parser = (" ", ",", ":", ";", "..", "(", ")", ":=")
-reserved = ("is", "begin", "end", "type", "range ", "array", "of", "procedure", "in", "out", "if", "then", "elsif", "else", "end", "loop", "while", "exit", "when")
+reserved = ("is", "begin", "end", "type", "range ", "array", "of", "procedure", "in", "out", "if", "then", "elsif", "else", "loop", "while", "exit", "when")
 
 relationalOperator = ("=" , "/=" , "<" , "<=" , ">" , ">=")
 binaryAddingOperator = ("+", "-")
